3583aabdfd9a30c0ad508238ac484f5a338b5e7d
Tanookerz <125132323+Tanookerz@users.noreply.github.com>
2023-03-01 11:16:46 +0800

    The original file itself.

---
+++ Mad_Libs_Project.py
@@ -0,0 +1,18 @@
+adj1=input("It was a ")
+adj2=input(",cold November day. I woke up to the ")
+bird=input("smell of ")
+room=input("roasting in the ")
+verb1=input("I ")
+verb2=input("down the stairs to see if I could help ")
+relative=input("the dinner. My mom said, \"See if ")
+noun1=input("needs a fresh ")
+liquid=input(".\" So I carried a tray of glasses full of ")
+verb3=input("into the ")
+bodyp=input("room. When I got there, I couldn't believe my ")
+noun2=input("There were ")
+verb4=input()
+noun3=input("on the ")
+print("It was a", adj1, ",cold November day. I woke up to", adj2, "smell of", bird, "roasting in the", room, "downstairs. I", verb1,
+    "down the stairs to see if I could help", verb2, "the dinner. My mom said, \"See if", relative, "needs a fresh", noun1
+    , ".\"So I carried a tray of glasses full of", liquid, "into the", verb3, "room. When I got there, I couldn't believe my"
+    , bodyp, "There were", noun2, verb4, "on the", noun3, "!")
